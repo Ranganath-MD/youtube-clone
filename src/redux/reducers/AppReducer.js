d639c936ff7dcb74822ed6a19cc6678db15b0af7
Ranganath <ranga@mverve.com>
2020-07-30 14:29:40 +0530

    Update AppReducer.js

--- src/redux/reducers/AppReducer.js
+++ src/redux/reducers/AppReducer.js
@@ -2,8 +2,7 @@ const initialState = {
     app: "ReTube",
     description: "Youtube clone using React",
     techstack: ["react", "redux", "sass", "axios"],
-    defaultChannelId: "UCW5YeuERMmlnqo4oq8vwUpg",
-    key: "AIzaSyCOW-2WzSf7l94gswQgrI9Xr8NZPReiN8g",
+    key: process.env.REACT_APP_YOUTUBE_KEY,
     part: "snippet",
 }
 
